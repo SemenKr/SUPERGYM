c05d9c4ea3236cb5cf10b4ef2f5602f916bf9ded
Семён Крекотун <samkrekotyn@gmail.com>
2022-12-21 23:03:25 +0300

    feat video

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,6 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
+import {initVideo} from './modules/media/video';
 
 // ---------------------------------
 
@@ -9,6 +10,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // ---------------------------------
 
   iosVhFix();
+  initVideo();
 
   // Modules
   // ---------------------------------
