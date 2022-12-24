f61a39ba7e578a62bbb59cd142badd3e53ed640c
SemenK <samkrekotyn@gmail.com>
2022-12-24 15:07:39 +0300

    features

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {initVideo} from './modules/media/video';
 import {initTabs} from './modules/tabs/tabs';
+import './modules/modals/swiper';
 
 // ---------------------------------
 
@@ -13,6 +14,7 @@ window.addEventListener('DOMContentLoaded', () => {
   iosVhFix();
   initVideo();
   initTabs();
+
 
   // Modules
   // ---------------------------------
---
+++ source/js/modules/modals/swiper.js
@@ -0,0 +1,34 @@
+import Swiper, {Navigation} from 'swiper';
+
+// init Swiper:
+const coachesSwiper = new Swiper('.coaches__slider', {
+  // configure Swiper to use modules
+  modules: [Navigation],
+  slidesPerView: 4,
+  slidesPerGroup: 1,
+  observer: true,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    autoHeight: true,
+  },
+});
